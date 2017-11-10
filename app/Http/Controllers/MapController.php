2afe376d36198876190af56ef1d54630bd1ac8b8
Stelio Tonica <srtstelio@yahoo.com.br>
2017-11-10 20:53:04 +0200

    first commit

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mapper;
+
+
+class MapController extends Controller
+{
+    
+
+    public function index()
+    {
+        Mapper::map(53.381128999999990000, -1.470085000000040000);
+
+        return view('cornford.googlmapper.map');
+    }
+}
